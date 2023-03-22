decfbcabf8887118abb54f4cec832f413f60c28d
kasdanb <127146581+kasdanb@users.noreply.github.com>
2023-03-22 10:26:28 -0700

    first commit

---
+++ main.py
@@ -0,0 +1,108 @@
+# Created By Kas
+# Agenda:
+# gIT GITHUB    
+# Build file and folder structures
+# Create libraries
+# testing github changes
+# I changed something - I changed something else tooooo!
+
+# This file was created by: Chris Cozort
+# Sources: http://kidscancode.org/blog/2016/08/pygame_1-1_getting-started/
+# Sources: 
+
+# import libs
+import pygame as pg
+import random
+import os
+# import settings 
+from settings import *
+from sprites import *
+# from pg.sprite import Sprite
+
+# set up assets folders
+game_folder = os.path.dirname(__file__)
+img_folder = os.path.join(game_folder, "img")
+
+def draw_text(text, size, color, x, y):
+    font_name = pg.font.match_font('arial')
+    font = pg.font.Font(font_name, size)
+    text_surface = font.render(text, True, color)
+    text_rect = text_surface.get_rect()
+    text_rect.midtop = (x,y)
+    screen.blit(text_surface, text_rect)
+
+def get_mouse_now():
+    x,y = pg.mouse.get_pos()
+    return (x,y)
+
+# init pg and create window
+pg.init()
+# init sound mixer
+pg.mixer.init()
+screen = pg.display.set_mode((WIDTH, HEIGHT))
+pg.display.set_caption("My first game...")
+clock = pg.time.Clock() 
+
+# i have a group of sprites, allows you to update all sprites at the same time
+all_sprites = pg.sprite.Group()
+enemies = pg.sprite.Group()
+
+player = Player()
+
+all_sprites.add(player)
+player.pos = (0,0)
+
+# creates a for loop that creates 20 mobs
+# 
+for i in range(0,20):
+    # instantiate mobs
+    m = Mob(randint(30,90), randint(30, 90), (255,0,0))
+    # add enemies to enemies and all_sprites...
+    enemies.add(m)
+    all_sprites.add(m)
+
+print(enemies)
+# game loop
+
+
+
+while RUNNING:
+    #  keep loop running at the right speed
+    clock.tick(FPS)
+    ### process input events section of game loop
+    for event in pg.event.get():
+        # check for window closing
+        if event.type == pg.QUIT:
+            RUNNING = False
+            # break
+    # print(get_mouse_now())
+    ### update section of game loop (if updates take longer the 1/30th of a second, you will get laaaaag...)
+    def input(self):
+        keystate = pg.key.get_pressed()
+        if keystate[pg.K_ESCAPE]:
+            print ('this is working')
+            if event.key
+            PAUSED == True
+    if PAUSED != True:
+        all_sprites.update()
+        enemies.update()
+
+    # if player hits enemies
+    blocks_hit_list = pg.sprite.spritecollide(player, enemies, True)
+    if blocks_hit_list:
+        SCORE += 1
+
+    for block in blocks_hit_list:
+        print(block)
+        pass
+    
+    ### draw and render section of game loop
+    screen.fill(BLUE)
+    all_sprites.draw(screen)
+    draw_text("Score: " + str(SCORE), 22, WHITE, WIDTH/2, HEIGHT/10)
+
+    # double buffering draws frames for entire screen
+    pg.display.flip()
+    # pg.display.update() -> only updates a portion of the screen
+# ends program when loops evaluates to false
+pg.quit()
---
+++ sprites.py
@@ -0,0 +1,94 @@
+import pygame as pg
+
+from pygame.sprite import Sprite
+
+from settings import *
+
+vec = pg.math.Vector2
+
+from random import randint
+
+# player class
+
+class Player(Sprite):
+    def __init__(self):
+        Sprite.__init__(self)
+        # these are the properties
+        self.image = pg.Surface((50,50))
+        self.image.fill(BLACK)
+        self.rect = self.image.get_rect()
+        self.rect.center = (WIDTH/2, HEIGHT/2)
+        self.pos = vec(WIDTH/2, HEIGHT/2)
+        self.vel = vec(0,0)
+        self.acc = vec(0,0)
+        self.cofric = 0.1
+        self.canjump = False
+    def input(self):
+        keystate = pg.key.get_pressed()
+        if keystate[pg.K_w]:
+            self.acc.y = -PLAYER_ACC
+        if keystate[pg.K_a]:
+            self.acc.x = -PLAYER_ACC
+        if keystate[pg.K_s]:
+            self.acc.y = PLAYER_ACC
+        if keystate[pg.K_d]:
+            self.acc.x = PLAYER_ACC
+    # ...
+    def inbounds(self):
+        if self.rect.x > WIDTH - 50:
+            self.pos.x = WIDTH - 25
+            self.vel.x = 0
+            print("i am off the right side of the screen...")
+        if self.rect.x < 0:
+            self.pos.x = 25
+            self.vel.x = 0
+            print("i am off the left side of the screen...")
+        if self.rect.y > HEIGHT:
+            print("i am off the bottom of the screen")
+        if self.rect.y < 0:
+            print("i am off the top of the screen...")
+
+    def update(self):
+        self.inbounds()
+        self.acc = self.vel * PLAYER_FRICTION
+        self.input()
+        self.vel += self.acc
+        self.pos += self.vel + 0.5 * self.acc
+        self.rect.center = self.pos
+
+class Mob(Sprite):
+    def __init__(self,width,height, color):
+        Sprite.__init__(self)
+        self.width = width
+        self.height = height
+        self.image = pg.Surface((self.width,self.height))
+        self.color = color
+        self.image.fill(self.color)
+        self.rect = self.image.get_rect()
+        self.rect.center = (WIDTH/2, HEIGHT/2)
+        self.pos = vec(WIDTH/2, HEIGHT/2)
+        self.vel = vec(randint(1,5),randint(1,5))
+        self.acc = vec(1,1)
+        self.cofric = 0.01
+    # ...
+    def inbounds(self):
+        if self.rect.x > WIDTH:
+            self.vel.x *= -1
+            # self.acc = self.vel * -self.cofric
+        if self.rect.x < 0:
+            self.vel.x *= -1
+            # self.acc = self.vel * -self.cofric
+        if self.rect.y < 0:
+            self.vel.y *= -1
+            # self.acc = self.vel * -self.cofric
+        if self.rect.y > HEIGHT:
+            self.vel.y *= -1
+            # self.acc = self.vel * -self.cofric
+    def update(self):
+        self.inbounds()
+        # self.pos.x += self.vel.x
+        # self.pos.y += self.vel.y
+        self.pos += self.vel
+        self.rect.center = self.pos
+
+
